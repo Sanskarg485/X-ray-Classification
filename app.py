66af8e61b330278125a09bd4ff97e95856934396
Sanskar gupta <146902492+Sanskarg485@users.noreply.github.com>
2025-07-08 15:56:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from PIL import Image
+import torch
+import torchvision.transforms as transforms
+from torchvision.models import resnet18
+import torch.nn as nn
+knn_model.pkl
+import pickle
+import numpy as np
